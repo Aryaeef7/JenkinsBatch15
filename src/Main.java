49547323207f5d9f4a3e514c0fe92f0b18249082
aryaeef7 <aryaeef7@gmail.com>
2023-05-30 21:19:27 -0400

    first commit555

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i <9; i++) {
             System.out.println("Hello world");
             System.out.println("I love M ");
 
